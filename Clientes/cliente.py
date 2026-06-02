a79d1d97e04cb84989ca0b1c7a39533701aec48f
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2026-06-02 22:23:26 +0000

    Adiciona regras de nome e cpf

---
+++ Clientes/cliente.py
@@ -0,0 +1,10 @@
+def cliente (nome, cpf, telefone, email):
+    if nome not null and len(nome) >= 5:
+        pass 
+    else:
+        print("Nome inválido.")
+
+    if cpf not null and len(cpf) == 11:
+        pass 
+    else: 
+        print("CPF inválido.")        
