7563d7527962e116ba4b4e7801ce49f7aacddb4d
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2026-06-02 22:39:10 +0000

    correção dos métodos de nome e cpf

--- Clientes/cliente.py
+++ Clientes/cliente.py
@@ -1,10 +1,38 @@
-def cliente (nome, cpf, telefone, email):
-    if nome not null and len(nome) >= 5:
-        pass 
-    else:
-        print("Nome inválido.")
+class Cliente:
+    def __init__(self, nome, cpf, telefone, email):
+        self.nome = nome
+        self.cpf = cpf
+        self.telefone = telefone
+        self.email = email 
+    
+    def nome():
+         if self.nome is None or self.nome == "" or len(self.nome) < 5:
+            print("Nome inválido.")
 
-    if cpf not null and len(cpf) == 11:
-        pass 
-    else: 
-        print("CPF inválido.")        
+    def cpf():
+        if self.cpf is None or self.cpf == "" or len(self.cpf) != 11:
+            print("CPF inválido.")
+
+    
+
+
+
+
+"""
+O telefone é opcional.
+
+Quando informado, deve possuir entre 10 e 11 dígitos numéricos.
+
+Caso um telefone inválido seja informado, o sistema não deve armazená-lo.
+
+E-mail
+O e-mail é opcional.
+
+Quando informado, deve conter o caractere '@'.
+
+Caso um e-mail inválido seja informado, o sistema não deve armazená-lo.
+
+"""
+
+
+    
