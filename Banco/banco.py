95ada122ccce428f0269f84335b8b33633f40ffa
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2026-06-02 23:11:49 +0000

    Adiciona classe Banco

---
+++ Banco/banco.py
@@ -0,0 +1,13 @@
+Class Banco:
+    def __init__(self, nome, codigo):
+        self.nome = nome
+        self.codigo = codigo
+        self.contas = []
+
+
+
+'''
+nome : String
+codigo : int
+contas : coleção de ContaBancaria
+'''
