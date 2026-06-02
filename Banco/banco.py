9326e06a7aad9d7c44a881387d8b109e867b4175
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2026-06-02 23:18:03 +0000

    Adiciona regras de negócio do banco

--- Banco/banco.py
+++ Banco/banco.py
@@ -4,10 +4,21 @@ Class Banco:
         self.codigo = codigo
         self.contas = []
 
+    def nome(self):
+        if self.nome is None or self.nome == "" or len(self.nome) < 3:
+            print("Nome do banco inválido.")
 
+    def codigo(self):
+        if self.codigo is None or self.codigo <= 0:
+            print("Código do banco inválido.")
 
 '''
-nome : String
-codigo : int
-contas : coleção de ContaBancaria
+Contas
+Um banco pode possuir várias contas.
+
+Não podem existir duas contas com o mesmo número dentro do mesmo banco.
+
+Uma conta somente pode pertencer a um banco por vez.
+
+O banco deve ser capaz de localizar contas a partir de seu número.
 '''
